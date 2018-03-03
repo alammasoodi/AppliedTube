fa057837b6dd1b2ff613e09f844af54c421cfa05
alammasoodi <alam@trialx.com>
2018-03-03 12:52:32 +0530

    voice search feature added

--- app/src/main/java/com/example/alam/appliedtube/SearchActivity.java
+++ app/src/main/java/com/example/alam/appliedtube/SearchActivity.java
@@ -1,15 +1,22 @@
 package com.example.alam.appliedtube;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Handler;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -17,13 +24,17 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
 
     private EditText searchInput;
     private ListView videosFound;
-
+    EditText ed;
+    TextView tv;
+    private static final int REQUEST_CODE = 1234;
+    Button speak;
     private Handler handler;
 
     @Override
@@ -31,24 +42,92 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        searchInput = (EditText)findViewById(R.id.search_input);
-        videosFound = (ListView)findViewById(R.id.videos_found);
-
+        searchInput = (EditText) findViewById(R.id.search_input);
+        ed = (EditText) findViewById(R.id.editText1);
+        tv = (TextView) findViewById(R.id.textView1);
+        videosFound = (ListView) findViewById(R.id.videos_found);
         handler = new Handler();
 
-        searchInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if(actionId == EditorInfo.IME_ACTION_DONE){
-                    searchOnYoutube(v.getText().toString());
-                    return false;
-                }
-                return true;
-            }
-        });
+//        searchInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+//            @Override
+//            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+//                if (actionId == EditorInfo.IME_ACTION_DONE) {
+//                    searchOnYoutube(v.getText().toString());
+//                    return false;
+//                }
+//                return true;
+//            }
+//        });
         addClickListener();
 
+        // Disable button if no recognition service is present
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
+        if (activities.size() == 0) {
+            speak.setEnabled(false);
+            speak.setText("Recognizer not present");
+        }
+        ed.addTextChangedListener(new TextWatcher() {
+            @
+                    Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // TODO Auto-generated method stub
+            }
+
+            @
+                    Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // TODO Auto-generated method stub
+            }
+
+            @
+                    Override
+            public void afterTextChanged(Editable s) {
+                // TODO Auto-generated method stub
+                speak.setEnabled(false);
+            }
+        });
     }
+    public void speakButtonClicked(View v)
+    {
+        startVoiceRecognitionActivity();
+    }
+    /**
+     * Fire an intent to start the voice recognition activity.
+     */
+    private void startVoiceRecognitionActivity()
+    {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Voice searching...");
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+    /**
+     * Handle the results from the voice recognition activity.
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK)
+        {
+            // Populate the wordsList with the String values the recognition engine thought it heard
+            final ArrayList< String > matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            if (!matches.isEmpty())
+            {
+                String Query = matches.get(0);
+                Log.i("query",""+Query);
+//                ed.setText(Query);
+//                speak.setEnabled(false);
+                searchOnYoutube(Query);
+
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+
+
     private List<VideoItem> searchResults;
 
     private void searchOnYoutube(final String keywords){
@@ -99,4 +178,7 @@ public class SearchActivity extends AppCompatActivity {
 
         });
     }
+
+
+
 }
